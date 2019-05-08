8a21119211e0d059dceccbccb5b184b9d3034e2a
Jarvislee7 <48046359+Jarvislee7@users.noreply.github.com>
2019-05-08 14:21:44 +0800

    Update duc.sh

--- duc.sh
+++ duc.sh
@@ -4,16 +4,19 @@ starttime=$(date +%Y-%m-%d\ %H:%M:%S)
 echo $starttime >> /home/admin/log/extendDucnet.log
 echo "start time: " $starttime
 
-for i in 95155 95156 95302 95303 95328 92586
+for i in 530560 530561 530562 531001 544084 95302 95328 96116 96133 96318 96502 502231 526710 528632 528687 528688 528968 530611 534862 543686  543687
 do
         echo "####################################################################"
         echo "client: " $i
         echo "curl https://ducttape-api11.prod.rdu1.west.isilon.com/api/v1.0/clusters/$i/extend" >> /home/admin/log/extendDucnet.log
         curl https://ducttape-api11.prod.rdu1.west.isilon.com/api/v1.0/clusters/$i/extend >> /home/admin/log/extendDucnet.log
         curl https://ducttape-api11.prod.rdu1.west.isilon.com/api/v1.0/clusters/$i/extend
+        curl https://ducttape.west.isilon.com/api/v2.0/clusters/$i/extend
+        curl https://ducttape.west.isilon.com/api/v2.0/clients/$i/extend
         echo "####################################################################"
         echo ""
 done
+
 
 
 #echo "curl https://ducttape-api11.prod.rdu1.west.isilon.com/api/v1.0/clusters/95155/extend" >> /home/admin/log/extendDucnet.log
